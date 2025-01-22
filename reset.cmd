251b35036d95ebfad8c4b36186cf201b2ad50812
realrank1 <ananthachaipimmaha0650284059@gmail.com>
2025-01-22 12:14:15 +0700

    Update reset.cmd

--- reset.cmd
+++ reset.cmd
@@ -10,5 +10,9 @@ ipconfig /renew
 cls
 taskkill /f /im "F Drop internet.exe"
 cls
+cd %systemroot%\system32
+call :IsAdmin
+Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\0001" /v "*SpeedDuplex" /t REG_SZ /d "0" /f
+cls
 exit
 cls
